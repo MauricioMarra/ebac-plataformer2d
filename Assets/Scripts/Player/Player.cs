6d9a360ba6ef05264bc07f039865a4cb1f2184e8
MauricioMarra <52969819+MauricioMarra@users.noreply.github.com>
2023-01-31 16:05:52 -0300

    feat:adds jumping and air time

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,16 +13,21 @@ public class Player : MonoBehaviour
     [Header("Jump")]
     public float jumpForce;
     public float jumpSpeed;
+    private bool isInAir = false;
 
     [Header("Animations")]
     public Animator playerAnimator;
     public string animatorKeyForRunning = "isRunning";
+    public string animatorKeyForJumpingVelocity = "jumpingVelocity";
+    public string _animatorTriggerForTouchedGround = "touchedGround";
 
     private float _currentSpeed;
     private float _runningSpeedFactor = 3;
     private float _scaleDuration = 0.5f;
     private bool isJumping = false;
+    private string floorTag = "Floor";
 
+    private bool myVar = true;
 
     private void Start()
     {
@@ -32,6 +37,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(myVar);
+
         Jump();
 
         Move();
@@ -62,14 +69,14 @@ public class Player : MonoBehaviour
         {
             //myRigidBody.velocity = myRigidBody.velocity + Vector2.left * Time.deltaTime * speed;
             myRigidBody.velocity = new Vector2(-_currentSpeed, myRigidBody.velocity.y);
-            this.playerAnimator.SetBool(animatorKeyForRunning, true);
+            this.playerAnimator.SetBool(animatorKeyForRunning, !isJumping && !isInAir);
             this.transform.DOScaleX(-1, _scaleDuration);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.velocity = myRigidBody.velocity + Vector2.right * Time.deltaTime * speed;
             myRigidBody.velocity = new Vector2(_currentSpeed, myRigidBody.velocity.y);
-            this.playerAnimator.SetBool(animatorKeyForRunning, true);
+            this.playerAnimator.SetBool(animatorKeyForRunning, !isJumping && !isInAir);
             this.transform.DOScaleX(1, _scaleDuration);
         }
         else
@@ -86,10 +93,44 @@ public class Player : MonoBehaviour
             isJumping = true;
             myRigidBody.velocity = Vector2.up * jumpForce;
         }
+
+        if (isJumping || isInAir)
+        {
+            if (myRigidBody.velocity.y > 0)
+                playerAnimator.SetFloat(animatorKeyForJumpingVelocity, myRigidBody.velocity.y);
+            else if (myRigidBody.velocity.y < 0)
+                playerAnimator.SetFloat(animatorKeyForJumpingVelocity, myRigidBody.velocity.y);
+        }
     }
 
     public void Respawn()
     {
         this.transform.position = restartPoint.transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isInAir)
+        {
+            if (collision.gameObject.CompareTag(floorTag))
+                playerAnimator.SetTrigger(_animatorTriggerForTouchedGround);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(floorTag))
+            isInAir = true;
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(floorTag))
+        {
+            isInAir = false;
+            isJumping = false;
+            playerAnimator.SetFloat(animatorKeyForJumpingVelocity, 0.0f);
+        }
     }
 }
